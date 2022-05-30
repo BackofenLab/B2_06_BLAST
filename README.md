7a4a2b5d52ced1a1123963d6c3ea410862404808
Alexander-Mitrofanov <mitrofan@informatik.uni-freiburg.de>
2022-05-30 19:04:33 +0200

    implemented tasks

--- README.md
+++ README.md
@@ -10,11 +10,13 @@ Lehrstuhl für Bioinformatik - Institut für Informatik - *http://www.bioinf.uni
 
 ### _Exercise 3 - Programming Assignment_
 In this exercise you will implement a simplified version of the BLAST algorithm.
-In these sheet we will be working with the protein sequences.
+Please note that the implementation is created for the educational purposes and only mimics the BLAST algorithm.
+Also note that in this sheet we will be working with the protein sequences.
 
 **a)** In order to start searching for the hits we need to get the similarity between amino acids.
 The first helper function will read the BLOSUM62 matrix from the file `BLOSUM62.txt` (https://www.ncbi.nlm.nih.gov/Class/FieldGuide/BLOSUM62.txt) and return a dictionary with the similarity between amino acids.
 The keys of the dictionary are the amino acids and the values are sub-dictionaries. The sub-dictionaries have the amino acids as keys and the similarity as values.
+
 
 <details>
   <summary>Example: (Spoiler)</summary>
@@ -23,6 +25,90 @@ The keys of the dictionary are the amino acids and the values are sub-dictionari
    >>> blosum62 = read_blosum62("BLOSUM62.txt")
    >>> print(blosum62)
    {'A': {'A': 4, 'R': -1, 'N': -2, 'D': -2, 'C': 0, 'Q': -1, 'E': -1, 'G': 0, 'H': -2, 'I': -1, 'L': -1, 'K': -1, 'M': -1, 'F': -2, 'P': -1, 'S': 1, 'T': 0, 'W': -3, 'Y': -2, 'V': 0, 'B': -2, 'Z': -1, 'X': 0, '*': -4}, 'R': {'A': -1, 'R': 5, 'N': 0, 'D': -2, 'C': -3, ...
+  ```
+
+</details>
+
+if you are puzzled with this step you can simply import the `convert_blosum_txt_to_dict_correct` from the helpers file
+```angular2html
+from helpers import convert_blosum_txt_to_dict_correct
+```
+
+**b)** In order to search for the similar sub-sequences between the database and the query we need to be able to index a sequence.
+Here you are asked to implement the `index_sequence_by_kmers` function. The function takes the sequence and the kmer length and returns a dictionary with the kmers as keys and the positions (list of indexes of kmers starts) as values. In biology the numeration starts with 1.
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+    >>>database = "DPPEGVVDPP"
+    >>>query = "RPPQGLF"
+
+    >>>indexes_db = index_sequence_by_kmers(database, 3)
+    >>>indexes_query = index_sequence_by_kmers(query, 3)
+    
+    >>>print(indexes_db)
+    >>>print(indexes_query)
+    
+    {'DPP': [1, 8], 'PPE': [2], 'PEG': [3], 'EGV': [4], 'GVV': [5], 'VVD': [6], 'VDP': [7]}
+    {'RPP': [1], 'PPQ': [2], 'PQG': [3], 'QGL': [4], 'GLF': [5]}
+  
+  ```
+
+</details>
+
+
+**c)** Now we need to search for similar kmers in the database and the query. In order to do so lets implement a couple of helpers.
+
+**c0)** List all existing kmers(use both database and query). Note that you can use the index function from the previous part and just the keys.
+
+**c1)** First lets try to list all kmers which are similar to the given one within the given threshold.
+In order to do that implement the `find_similar_kmers_for_kmer` which takes four arguments: kmer, kmer similarity threshold, all existing kmers and blosum dictionary.
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+    >>>all_similar_kmers = find_similar_kmers_for_kmer("PQG", 13, all_existing_kmers, dict_blosum)
+    >>>print(all_similar_kmers)
+ 
+    ['PEG', 'PQG']
+  
+  ```
+
+</details>
+
+
+**c2)** Now we can implement the function which will of all similar kmers in the sequence for each kmer in the sequence.
+In order to do that implement the `find_similar_kmers_for_sequence` which takes three arguments: sequence, kmer similarity threshold and blosum dictionary.
+This function should return a dictionary with the kmer as key and the list of similar kmers as value.
+
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+    >>>all_similar_kmers_for_sequence = find_similar_kmers_for_sequence_correct("DPPEGVVDPP", 13, all_existing_kmers, dict_blosum)
+    >>>print(all_similar_kmers_for_sequence)
+ 
+    {'DPP': ['DPP'], 'PPE': ['PPE', 'PPQ'], 'PEG': ['PEG', 'PQG'], 'EGV': ['EGV'], 'GVV': ['GVV'], 'VVD': ['VVD'], 'VDP': ['VDP']}
+  
+  ```
+
+</details>
+
+**c3)** Finally we are ready to find the hits in both database and query sequences.
+In order to do that implement the `create_index_pairs_correct` function. The function has 5 arguments as the input: query sequence, database sequence, kmer_size, kmer similarity threshold and the blosum dictionary.
+The function returns a dictionary with kmers as keys and tuples of indexes as values. Each tuple has the list of indexes of the kmer in the query sequence as the first value and a list of indexes of the similar tuples is the database as the second value.
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+    >>>dict_both_indexes = create_index_pairs_correct(query, database, 3, 5, dict_blosum)
+    >>>print(dict_both_indexes)print(dict_both_indexes)
+    
+    {'RPP': ([1], [1, 8]), 'PPQ': ([2], [1, 8, 2]), 'PQG': ([3], [3]), 'QGL': ([4], [4]), 'GLF': ([5], [5])}
   ```
 
 </details>
--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,7 +1,7 @@
 # In this file you can find sorted helpers for the BLAST implementation
 
 
-# 1. Reading the blossum file
+# 1. Reading the blosum file
 
 def convert_blosum_txt_to_dict_correct(filepath):
     """
@@ -36,18 +36,6 @@ def index_sequence_by_kmers_correct(sequence, k):
 # 3. Searching for all similar kmers with a given threshold
 
 
-def generate_all_possible_kmers_of_size_k_correct(k, dict_blosum):
-    """
-    Generate all possible kmers of size k.
-    """
-    all_possible_characters = list(dict_blosum.keys())
-    all_possible_kmers = [""]
-    for _ in range(k):
-        all_possible_kmers = [word_ + character for word_ in all_possible_kmers
-                              for character in all_possible_characters]
-    return all_possible_kmers
-
-
 def find_similar_kmers_for_kmer_correct(kmer, kmer_similarity_threshold, all_possible_kmers, dict_blosum):
     """
     Find similar words.
@@ -75,14 +63,14 @@ def find_similar_kmers_for_sequence_correct(sequence, kmer_similarity_threshold,
     return dict_similar_kmers
 
 
-def create_index_pairs(query_sequence, database_sequence, kmer_size, kmer_similarity_threshold, dict_blosum):
+def create_index_pairs_correct(query_sequence, database_sequence, kmer_size, kmer_similarity_threshold, dict_blosum):
     """
     Create an index of the query sequence and the database sequence.
     """
     dict_both_indexes = {}
     indexed_database_sequence = index_sequence_by_kmers_correct(database_sequence, kmer_size)
     indexed_query_sequence = index_sequence_by_kmers_correct(query_sequence, kmer_size)
-    all_possible_kmers = generate_all_possible_kmers_of_size_k_correct(kmer_size, dict_blosum)
+    all_possible_kmers = list(indexed_database_sequence.keys()) + list(indexed_query_sequence.keys())
     dict_similar_kmers_query = find_similar_kmers_for_sequence_correct(query_sequence, kmer_similarity_threshold,
                                                                all_possible_kmers, dict_blosum)
     for kmer, kmer_indexes_query in indexed_query_sequence.items():
@@ -95,18 +83,133 @@ def create_index_pairs(query_sequence, database_sequence, kmer_size, kmer_simila
     return dict_both_indexes
 
 
+# 4. Merging hits
+
+def merge_single_hit_with_single_hit(single_hit1, singe_hit2, max_distance):
+    """
+    Merge two extended hits.
+    """
+    dist_first = abs(single_hit1[0] - singe_hit2[0]) - 1
+    dist_second = abs(single_hit1[1] - singe_hit2[1]) - 1
+    if dist_first + dist_second <= max_distance:
+        return True, sorted([single_hit1, singe_hit2])
+    return False, None
+
+
+def merge_multiple_hits_with_single_hit(multiple_hits, single_hit, max_distance):
+    """
+    Merge multiple hits with a single hit.
+    """
+    for hit in multiple_hits:
+        flag_can_be_merged, new_multiple_hits = merge_single_hit_with_single_hit(hit, single_hit, max_distance)
+        if flag_can_be_merged:
+            multiple_hits.append(single_hit)
+            return True, sorted(multiple_hits)
+    return False, None
+
+
+def merge_two_extended_hits(extended_hit1, extended_hit2, max_distance):
+    """
+    Merge two extended hits.
+    """
+    if (type(extended_hit1) == tuple) and (type(extended_hit2) == tuple):
+        can_be_merged, merged = merge_single_hit_with_single_hit(extended_hit1, extended_hit2, max_distance)
+        return can_be_merged, merged
+    elif type(extended_hit1) == list and type(extended_hit2) == tuple:
+        can_be_merged, merged = merge_multiple_hits_with_single_hit(extended_hit1, extended_hit2, max_distance)
+        return can_be_merged, merged
+    elif type(extended_hit1) == tuple and type(extended_hit2) == list:
+        can_be_merged, merged = merge_multiple_hits_with_single_hit(extended_hit2, extended_hit1, max_distance)
+        return can_be_merged, merged
+    else:
+        for single_hit in extended_hit1:
+            can_be_merged, merged = merge_single_hit_with_single_hit(extended_hit2, single_hit, max_distance)
+            if can_be_merged:
+                merged = extended_hit1 + extended_hit2
+                return True, sorted(merged)
+    return False, None
+
+
+def merging_one_iteration(list_extended_hits, max_distance):
+    """
+    Merge hits in one iteration.
+    """
+    for i in range(len(list_extended_hits) - 1):
+        for j in range(i + 1, len(list_extended_hits)):
+            can_be_merged, merged = merge_two_extended_hits(list_extended_hits[i], list_extended_hits[j], max_distance)
+            if can_be_merged:
+                first_element_to_remove = list_extended_hits[i]
+                second_element_to_remove = list_extended_hits[j]
+                list_extended_hits.remove(first_element_to_remove)
+                list_extended_hits.remove(second_element_to_remove)
+                list_extended_hits.append(merged)
+                return False, list_extended_hits
+    return True, list_extended_hits
+
+
+def create_extended_hits_correct(query_sequence, database_sequence, kmer_size,
+                                 kmer_similarity_threshold, dict_blosum, max_distance):
+    dict_index_pairs = create_index_pairs_correct(query_sequence, database_sequence,
+                                                  kmer_size, kmer_similarity_threshold, dict_blosum)
+
+    flat_list_hits = []
+    for kmer, (kmer_indexes_query, indexes_database_all_similar) in dict_index_pairs.items():
+        all_pairs = [(i, j) for i in kmer_indexes_query for j in indexes_database_all_similar]
+        flat_list_hits.extend(all_pairs)
+
+    print(flat_list_hits)
+
+    list_extended_hits = flat_list_hits
+    flag_all_potential_merges_are_done = False
+    while not flag_all_potential_merges_are_done:
+        flag_all_potential_merges_are_done, list_extended_hits = merging_one_iteration(list_extended_hits, max_distance)
+
+    return list_extended_hits
 
 
 def main():
+
+    ###############  1 Start ################
     dict_blosum = convert_blosum_txt_to_dict_correct("BLOSUM62.txt")
     print(dict_blosum)
+
+    ###############  1 End ################
+
+    ###############  2 Start ################
 
     database = "DPPEGVVDPP"
     query = "RPPQGLF"
 
-    dict_both_indexes = create_index_pairs(query, database, 3, 12, dict_blosum)
+    indexes_db = index_sequence_by_kmers_correct(database, 3)
+    indexes_query = index_sequence_by_kmers_correct(query, 3)
+
+    print(indexes_db)
+    print(indexes_query)
+
+    ###############  2 End ################
+
+
+    ###############  3 Start ################
+
+    all_existing_kmers = list(indexes_db.keys()) + list(indexes_query.keys())
+    all_similar_kmers = find_similar_kmers_for_kmer_correct("PQG", 13, all_existing_kmers, dict_blosum)
+    print(all_similar_kmers)
+
+    all_similar_kmers_for_sequence = find_similar_kmers_for_sequence_correct("DPPEGVVDPP", 13, all_existing_kmers, dict_blosum)
+    print(all_similar_kmers_for_sequence)
+
+    dict_both_indexes = create_index_pairs_correct(query, database, 3, 5, dict_blosum)
     print(dict_both_indexes)
 
+    ###############  3 End ################
+
+
+
+
+
+
+    extended_hits = create_extended_hits_correct(query, database, 3, 12, dict_blosum, 2)
+    print(extended_hits)
 
 
 if __name__ == "__main__":
